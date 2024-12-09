50dfad6a2cfbe4b8f1f8e9aa9891c0f6be065862
youths <pishonTron>
2024-12-09 23:34:47 +0800

    init dist

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', // or 'media'
-  content: ['./index.html', './src/**/*.{vue,ts,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
